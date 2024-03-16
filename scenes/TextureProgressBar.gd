d222b144ad9659850eb67c831855046974f7af0e
PatrickZochAlves <93534366+PatrickZochAlves@users.noreply.github.com>
2024-03-16 16:29:53 +0100

    blood effect for broyeuse

--- scenes/TextureProgressBar.gd
+++ scenes/TextureProgressBar.gd
@@ -1,10 +1,19 @@
 extends TextureProgressBar
 
+var global_capital = 0
+var _death_effect: PackedScene = preload("res://scenes/vfx/blood_effect.tscn")
+
+func generate_death(capital):
+	#Create effect
+	var effect = _death_effect.instantiate()
+	add_child(effect)
+	#GLOBAl SCORE
+	global_capital += capital
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	value = 0
-	max_value = 100
+	max_value = 200
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -12,6 +21,7 @@ func _process(delta):
 	value += 1
 	if value == max_value && $Timer.is_stopped():
 		$Timer.start()
+		generate_death(1)
 
 
 func _on_timer_timeout():
---
+++ scenes/vfx/blood_effect.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$broyeuse.play()
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_timer_timeout():
+	$crie.play()
+	$blood.emitting = true
