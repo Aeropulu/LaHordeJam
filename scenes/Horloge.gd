4dd3709ecb98040045d3052cd28407be2761caa5
PatrickZochAlves <93534366+PatrickZochAlves@users.noreply.github.com>
2024-03-16 20:08:07 +0100

    Update Horloge.gd

--- scenes/Horloge.gd
+++ scenes/Horloge.gd
@@ -3,10 +3,12 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	FactoryManager.remaining_day_time_changed.connect(on_update)
 
+func on_update(new_value):
+	$fleche.global_rotation_degrees = new_value*-360
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-	$fleche.global_rotation_degrees += 0.1
+	
