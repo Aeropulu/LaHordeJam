b2685851d3b9776e43976b774ce809a6ab5bae4a
Aeropulu <42248174+Aeropulu@users.noreply.github.com>
2024-03-16 12:30:51 +0100

    some work on machines

--- scripts/machine.gd
+++ scripts/machine.gd
@@ -2,6 +2,7 @@ extends Node2D
 class_name  Machine
 
 var machine_parameters # resource type
+var _worker_scene: PackedScene = preload("res://scenes/test_character.tscn")
 
 @export var _cycle_duration: float = 1.0
 var _current_worker: int = 0
@@ -9,6 +10,7 @@ var _current_worker: int = 0
 
 var _time_per_worker: float = 1.0
 var _time_to_next_worker: float = 1.0
+var _slot_count = 1
 
 signal has_produced # maybe this should go in the machine_parameters?
 
@@ -24,13 +26,31 @@ func _physics_process(delta):
 	_time_to_next_worker -= delta
 	if _time_to_next_worker <= 0:
 		_time_to_next_worker += _time_per_worker
-		_workers[_current_worker].work
+		_workers[_current_worker].work()
 		_produce()
 		_current_worker += 1
+		if _current_worker >= _workers.size():
+			_current_worker = 0
 
 func _remaining_workers() -> int:
 	return _workers.size() - _current_worker
 	
 func _produce() -> void:
-	machine_parameters.produce()
+	# machine_parameters.produce()
 	has_produced.emit()
+	
+func _add_worker(worker) -> bool:
+	var slots_count = %Slots.get_child_count()
+	if _workers.size() >= slots_count:
+		return false
+	var slot = %Slots.get_child(_workers.size())
+	slot.add_child(worker)
+	_workers.append(worker)
+	_time_per_worker = _cycle_duration / _workers.size()
+	return true
+
+
+func _on_area_2d_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton and event.pressed:
+		var worker = _worker_scene.instantiate()
+		_add_worker(worker)
--- scripts/test_character.gd
+++ scripts/test_character.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @onready var _navigation_agent: NavigationAgent2D = %NavigationAgent
+var particles_scene = preload("res://scenes/Particles/work_ping.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -10,10 +11,5 @@ func _ready():
 func _process(delta):
 	pass
 
-func _unhandled_input(event):
-	if event is InputEventMouseButton and event.is_pressed():
-		var new_pos: Vector2 = event.global_position
-		print(event.global_position)
-		_navigation_agent.set_target_position(new_pos)
-		print(_navigation_agent.is_target_reachable())
-		print(_navigation_agent.target_position)
+func work():
+	add_child(particles_scene.instantiate())
