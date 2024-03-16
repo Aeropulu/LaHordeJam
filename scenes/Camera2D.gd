816fe047e77f44520a0a3b5de957a8fe9fd5ad22
PatrickZochAlves <93534366+PatrickZochAlves@users.noreply.github.com>
2024-03-16 18:12:24 +0100

    positioning

---
+++ scenes/Camera2D.gd
@@ -0,0 +1,20 @@
+extends Camera2D
+
+var _signal = 1
+var frequency = 8.0
+var shakingDuration = 30
+var shakingTimer = 0
+
+func _ready():
+	pass # Replace with function body.
+
+func _process(delta):
+	if shakingTimer > 0 :#&& (global_position.y == 432 || global_position.y == 0):
+		if fmod(shakingTimer, frequency) == 0:
+			var horizontalAmplitude = randf_range(2.0,3.0)
+			var verticalAmplitude = randf_range(2.0,3.0)
+			_signal *= -1
+			set_offset(Vector2(horizontalAmplitude*_signal + 960,verticalAmplitude*_signal + 540))
+		shakingTimer -= 1
+	else:
+		set_offset(Vector2(960,540))
