9d1dc1db8fc5f714d772dd3b67ac2c98a2c27cd5
PatrickZochAlves <93534366+PatrickZochAlves@users.noreply.github.com>
2024-03-16 14:25:25 +0100

    added node profit

---
+++ scenes/Profit.gd
@@ -0,0 +1,28 @@
+extends Node2D
+
+var global_profit = 1000
+
+
+func generate_profit(profit):
+	#VFX
+	
+	
+	#SFX
+	$profitSFX.play()
+	
+	#GLOBAl SCORE
+	global_profit += profit
+	$profitLabel.text = str(global_profit)
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$profitLabel.text = str(global_profit)
+	
+func _unhandled_input(event):
+	if event is InputEventMouseButton and event.pressed:
+		generate_profit(1000)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
