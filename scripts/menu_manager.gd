602d393dd0682ed795020113ea167f18c780e6fc
PatrickZochAlves <93534366+PatrickZochAlves@users.noreply.github.com>
2024-03-17 12:32:41 +0100

    added buttons, flow, menus illustrations

--- scripts/menu_manager.gd
+++ scripts/menu_manager.gd
@@ -8,6 +8,8 @@ func _ready():
 	$menu_config.visible = false
 	$menu_debriefing.visible = false
 	$menu_gameover.visible = false
+	FactoryManager.day_end.connect(on_day_end)
+	FactoryManager.game_over.connect(on_game_over)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -27,3 +29,9 @@ func _on_launch_config_pressed():
 
 func _on_pointeuse_pointeuse_finished():
 	$menu_config.visible = false
+	
+func on_day_end():
+	$menu_debriefing.visible = true
+	
+func on_game_over():
+	$menu_gameover.visible = true
