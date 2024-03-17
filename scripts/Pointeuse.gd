ad96d502257f93dba514f4a2888b6f78e0494776
PatrickZochAlves <93534366+PatrickZochAlves@users.noreply.github.com>
2024-03-17 11:44:33 +0100

    menu, bouton

--- scripts/Pointeuse.gd
+++ scripts/Pointeuse.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+signal pointeuse_finished
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -16,6 +17,8 @@ func launch_journey():
 	$pointerSFX.play()
 	await $pointerSFX.finished
 	$ding.play()
+	await $ding.finished
+	pointeuse_finished.emit()
 	FactoryManager._on_button_pressed()
 
 func _on_area_2d_input_event(viewport, event, shape_idx):
--- scripts/menu_manager.gd
+++ scripts/menu_manager.gd
@@ -3,9 +3,27 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$menu_main.visible = true
+	$menu_briefing.visible = false
+	$menu_config.visible = false
+	$menu_debriefing.visible = false
+	$menu_gameover.visible = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_start_game_pressed():
+	$menu_main.visible = false
+	$menu_briefing.visible = true
+
+
+func _on_launch_config_pressed():
+	$menu_briefing.visible = false
+	$menu_config.visible = true
+	
+
+func _on_pointeuse_pointeuse_finished():
+	$menu_config.visible = false
